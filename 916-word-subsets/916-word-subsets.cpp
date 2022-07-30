e8b7992d2fb4bb84c92822444932cbfa3f925a80
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2022-07-30 11:11:42 +0530

    Time: 304 ms (56.18%), Space: 102.4 MB (40.67%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    
+      vector<int> counter(string& word) {
+        vector<int> count(26);
+        for (char c : word) count[c - 'a']++;
+        return count;
+    }
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+       vector<int> count(26), tmp(26);
+        int i;
+        for (string b : words2) {
+            tmp = counter(b);
+            for (i = 0; i < 26; ++i)
+            {
+                count[i] = max(count[i], tmp[i]);
+            }
+        }
+        vector<string> res;
+        for (string a : words1) {
+            tmp = counter(a);
+            for (i = 0; i < 26; ++i)
+                if (tmp[i] < count[i])
+                    break;
+            if (i == 26) res.push_back(a);
+        }
+        return res;
+    }
+};
