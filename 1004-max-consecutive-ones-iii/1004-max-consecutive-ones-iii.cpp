fbb18597716c21597c8814616c93d23ddee6fe05
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2022-08-17 16:38:10 +0530

    Time: 156 ms (5.39%), Space: 55.4 MB (65.65%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,45 +5,18 @@ public:
         
         int i =0, j =0, ans =0, ct=0;
         
-    if(k == 0)
-      {
-        for(int i = 0 ; i < n ; i++)
-            {
-                if(nums[i] == 1)
-                    ct++;
-                else{
-                    ans = max(ans, ct);
-                    ct=0;
-                }
-            }
-          return max(ct,ans);
-      }
-        
-        if(k == 1 && n == 1){
-            return 1;
-        }
-        
-        ans =0;  
-        map<int,int>mp;
-            
         while(j<n)
         {
-            mp[nums[j]]++;
+            if(nums[j] == 0)
+                ct++;
             
-            if(mp.size() == 2){
-                auto it = mp.find(0);
-                if(it->second <= k){
-                    ans = max(ans, j-i+1);
-                } else{
-                    while(it->second > k)
-                    {
-                        mp[nums[i]]--;
-                        if(mp[nums[i]] == 0)
-                            mp.erase(nums[i]);
-                        i++;
-                    }
-                }
+            while(ct>k)
+            {
+                if(nums[i] == 0)
+                    ct--;
+                i++;
             }
+            ans = max(ans,j-i+1);
             j++;
         }
         return ans;
