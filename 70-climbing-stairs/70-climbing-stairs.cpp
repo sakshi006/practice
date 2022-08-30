de62f19f415c1d0beaf1a74bf8947298a211ec5e
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2022-08-30 19:37:42 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.75%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     
-    int getcount(int n, int idx, vector<int>&dp)
-    {
-        if(idx>n)
-            return 0;
-        if(idx == n)
-            return 1;
+//     int getcount(int n, int idx, vector<int>&dp)
+//     {
+//         if(idx>n)
+//             return 0;
+//         if(idx == n)
+//             return 1;
         
-        if(dp[idx]!=-1)
-            return dp[idx];
+//         if(dp[idx]!=-1)
+//             return dp[idx];
         
-        return dp[idx] = getcount(n,idx+1,dp)+getcount(n,idx+2,dp);
+//         return dp[idx] = getcount(n,idx+1,dp)+getcount(n,idx+2,dp);
         
-    }
+//     }
     
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return getcount(n,0,dp);
+        if(n==1)
+            return 1;
+        vector<int>dp(n,0);
+        dp[0]=1;
+        dp[1] =2;
+        
+        for(int i = 2;i<n;i++)
+            dp[i] = dp[i-1]+dp[i-2];
+        
+        return dp[n-1];
     }
 };
