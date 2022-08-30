e72f76ea8db1560f7a590693e580384db95aaa92
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2022-08-30 19:45:33 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.47%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -16,15 +16,18 @@ public:
 //     }
     
     int climbStairs(int n) {
-        if(n==1)
-            return 1;
-        vector<int>dp(n,0);
-        dp[0]=1;
-        dp[1] =2;
+        if(n<=2)
+            return n;
         
-        for(int i = 2;i<n;i++)
-            dp[i] = dp[i-1]+dp[i-2];
+        int prev = 2,prev2=1, curr;
+    
+        for(int i = 3;i<=n;i++)
+        {
+            curr = prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
         
-        return dp[n-1];
+        return curr;
     }
 };
