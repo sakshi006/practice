16f106ad27a92995460281fae219b9177b0f50c2
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2022-08-30 22:31:18 +0530

    Time: 34 ms (55.26%), Space: 13.7 MB (68.76%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,52 +1,46 @@
 class Solution {
+public:
     
-    bool detectCycle(int node, int prev, vector<int>adj[], vector<int>&vis, vector<int>&dfsvis)
+    bool dfs(int node, int parent, vector<int>&vis, vector<int>&dfsvis, vector<int>adj[])
     {
-        vis[node] = 1;
-        dfsvis[node] = 1;
-     
-        for(auto it : adj[node])
+        vis[node]=1;
+        dfsvis[node] =1;
+        
+        for(auto it: adj[node])
         {
             if(!vis[it])
             {
-                if(detectCycle(it, node,  adj, vis, dfsvis))
-                {
+                if(dfs(it,node,vis,dfsvis,adj))
                     return true;
-                }
             }
-            else if (dfsvis[it]){
-                {
-                    return true;
-                }
-            }
+            else if(dfsvis[it])
+                return true;
         }
-        
-        dfsvis[node]=0;
+        dfsvis[node] = 0;
         return false;
     }
     
-public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
         
         vector<int>adj[numCourses+1];
         
-        for(int i =0;i<prerequisites.size();i++)
+        for(int i =0;i<n;i++)
         {
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
-
+            
             adj[v].push_back(u);
         }
         
         vector<int>vis(numCourses+1,0);
         vector<int>dfsvis(numCourses+1,0);
         
-        for(int i = 0 ; i < numCourses; i++)
+        for(int i =0;i<numCourses;i++)
         {
             if(!vis[i])
-            {
-                if(detectCycle(i, -1, adj, vis,dfsvis)) return false;
-            }
+                if(dfs(i,-1,vis,dfsvis,adj))
+                    return false;
         }
         
         return true;
