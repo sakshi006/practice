fd8974c1df39df729564a71064a659e00b74b5e3
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2022-09-18 23:00:08 +0530

    Time: 4 ms (36.92%), Space: 7.9 MB (25.97%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     
-    int maxProfit(vector<int>&nums, int idx, vector<int>&dp)
+    int robfunc(int idx, vector<int>& nums, vector<int>&dp)
     {
-        if(idx >= nums.size())
-            return 0;
-       
-        if(dp[idx] != -1)
+       if(idx == 0)
+       {
+           return nums[idx];
+       }
+       if(idx == 1)
+       {
+           return max(nums[0],nums[1]);
+       }
+        
+        if(dp[idx]!=-1)
             return dp[idx];
         
-        return dp[idx] = max((maxProfit(nums, idx+2, dp) + nums[idx]), maxProfit(nums, idx+1, dp));
+        int notpick = robfunc(idx-1, nums,dp);
+        int pick = 0;
+        
+        if(idx>1)
+            pick = nums[idx] + robfunc(idx-2, nums, dp);
+        
+        return dp[idx] = max(pick, notpick);
     }
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, -1);
-        return maxProfit(nums, 0, dp);
+        vector<int>dp(n+1,-1);
+        
+        return robfunc(n-1, nums, dp);
     }
 };
